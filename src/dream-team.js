7142182fe69f37337390a90a3310571fdef5e272
lyana <lyana-m@yandex.ru>
2021-02-24 13:08:04 +0500

    function implementation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members.reduce((acc, item) => {
+      if (typeof (item) === 'string') {
+        return acc + item.trim().toLowerCase()[0];
+      } else {
+        return acc;
+      }
+    }, '')
+      .split('').sort().join('').toUpperCase();
+  } return false;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date && !isNaN(date.valueOf())) {
+    let month = date.getMonth();
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return 'winter';
+        break;
+      case 2:
+      case 3:
+      case 4:
+        return 'spring';
+        break;
+      case 5:
+      case 6:
+      case 7:
+        return 'summer';
+        break;
+      case 8:
+      case 9:
+      case 10:
+        return 'autumn';
+        break;
+    }
+  } else {
+    return 'Unable to determine the time of year!';
+  }
 };
