4352ba2f5ac046d1401a66e38f69ac240fb04b75
lyana <lyana-m@yandex.ru>
2021-02-25 10:15:16 +0500

    function implementation

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push(value === undefined ? '( )' : `( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (isNaN(position) || !Number.isInteger(position) || position < 0 || position > this.result.length) {
+      this.result = [];
+      throw new Error('error');
+    } 
+    this.result.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let b = [...this.result];
+    this.result = [];
+    return b.join('~~');
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let transformedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if(arr[i-2] !== '--discard-next'){
+          transformedArr.pop();
+        }
+        break;
+      case '--double-next':
+        if(arr[i+1] !== undefined){
+          transformedArr.push(arr[i+1]);
+        }
+        break;
+      case '--double-prev':
+        if(arr[i-1] !== undefined && arr[i-2] !== '--discard-next'){
+          transformedArr.push(arr[i-1]);
+        }
+        break;
+      default:
+        transformedArr.push(arr[i]);
+        break;
+    }
+  }
+    return transformedArr;
 };
