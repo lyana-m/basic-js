31677cf842f58416eafc34c13c77c549bb242d37
lyana <lyana-m@yandex.ru>
2021-02-26 18:42:00 +0500

    function implementation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+
+  let additionArr = [];
+
+  if (options.repeatTimes == undefined) {
+    return str + options.addition;
+  }
+
+  for (i = 0; i < options.additionRepeatTimes; i++) {
+    additionArr.push(typeof (options.addition) === 'string' ? options.addition : String(options.addition));
+  }
+
+  let mainStr = (typeof (str) === 'string' ? str : String(str)) + additionArr.join(options.additionSeparator === undefined ? '|' : options.additionSeparator);
+  let mainArr = [];
+
+  for (i = 0; i < options.repeatTimes; i++) {
+    mainArr.push(mainStr);
+  }
+
+  return mainArr.join(options.separator === undefined ? '+' : options.separator);
+}
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(flag) {
+    this.flag = flag;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  }
+
+  encrypt(message, key) {
+
+    if (message === undefined || key === undefined) {
+      throw new Error('Argument absence');
+    }
+
+    let openMessage = message.toUpperCase().split('');
+
+    if (key.length < message.length) {
+      key = key.repeat(Math.ceil(message.length / key.length)).slice(0, message.length);
+    };
+
+    let keyWord = key.toUpperCase().split('');
+    let newArr = [];
+    let j = 0;
+    for (let i = 0; i < openMessage.length; i++) {
+      if (!this.alphabet.includes(openMessage[i])) {
+        newArr.push(`${openMessage[i]}`)
+        j--;
+      } else {
+        newArr.push(this.alphabet.find(letter => this.alphabet.indexOf(letter) === (this.alphabet.indexOf(openMessage[i]) + this.alphabet.indexOf(keyWord[j])) % 26))
+      }
+      j++;
+    }
+
+    return this.flag === true || this.flag === undefined ? newArr.join('') : newArr.reverse().join('');
+
+  }
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Argument absence');
+    }
+
+    let encryptedMessageArr = encryptedMessage.toUpperCase().split('');
+
+    if (key.length < encryptedMessage.length) {
+      key = key.repeat(Math.ceil(encryptedMessage.length / key.length)).slice(0, encryptedMessage.length);
+    };
+
+    let keyWord = key.toUpperCase().split('');
+    let newArr = [];
+    let j = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (!this.alphabet.includes(encryptedMessage[i])) {
+        newArr.push(`${encryptedMessage[i]}`)
+        j--;
+      } else {
+        newArr.push(this.alphabet.find(letter => this.alphabet.indexOf(letter) === (this.alphabet.indexOf(encryptedMessage[i]) - this.alphabet.indexOf(keyWord[j]) + 26) % 26));
+      }
+      j++;
+    }
+    return this.flag === true || this.flag === undefined ? newArr.join('') : newArr.reverse().join('');
   }
 }
 
